fe104f952d7940fb8aec10bcc6ca75e2524ca779
eswar-7116 <eswardudi06@gmail.com>
2025-12-10 22:13:15 +0530

    Add ExtractZIP function with tests and mock data

---
+++ internal/util/config.go
@@ -0,0 +1,5 @@
+package util
+
+const (
+	FunctionsDir = "./functions"
+)
---
+++ internal/util/extract.go
@@ -0,0 +1,61 @@
+package util
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func ExtractZIP(zipFilePath string) (string, error) {
+	base := filepath.Base(zipFilePath)
+	funcName := strings.TrimSuffix(base, filepath.Ext(base))
+	destDir := filepath.Join(FunctionsDir, funcName)
+
+	if err := os.MkdirAll(destDir, 0755); err != nil {
+		return "nil", fmt.Errorf("error creating function dir: %w", err)
+	}
+
+	r, err := zip.OpenReader(zipFilePath)
+	if err != nil {
+		return "", fmt.Errorf("error opening zip: %w", err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		fpath := filepath.Join(destDir, f.Name)
+
+		if !strings.HasPrefix(fpath, filepath.Clean(destDir)+string(os.PathSeparator)) {
+			return "", fmt.Errorf("illegal file path: %s", f.Name)
+		}
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, f.Mode())
+			continue
+		}
+
+		if err := os.MkdirAll(filepath.Dir(fpath), 0755); err != nil {
+			return "", fmt.Errorf("error extracting zip: %w", err)
+		}
+
+		dst, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return "", fmt.Errorf("error extracting zip: %w", err)
+		}
+		defer dst.Close()
+
+		src, err := f.Open()
+		if err != nil {
+			return "", fmt.Errorf("error extracting zip: %w", err)
+		}
+		defer src.Close()
+
+		if _, err = io.Copy(dst, src); err != nil {
+			return "", fmt.Errorf("error extracting zip: %w", err)
+		}
+	}
+
+	return destDir, nil
+}
