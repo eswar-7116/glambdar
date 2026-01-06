7a1f5fb58ce6f34c31305662dedb1fe253366da1
eswar-7116 <eswardudi06@gmail.com>
2026-01-06 14:03:28 +0530

    fix: create functions dir if not exists

--- internal/util/config.go
+++ internal/util/config.go
@@ -19,6 +19,17 @@ func InitPaths() error {
 	}
 
 	FunctionsDir = filepath.Join(BaseDir, "functions")
+
+	_, err := os.Stat(FunctionsDir)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(FunctionsDir, 0755)
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
 	WorkerPath = filepath.Join(BaseDir, "worker", "glambdar-worker.js")
 	return nil
 }
